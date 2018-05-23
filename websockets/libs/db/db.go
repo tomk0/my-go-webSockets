e3bb8ce619eaf53c2fda67eda634a10ae54c60ad
tomk0 <tomkelly16111996@gmail.com>
2018-05-23 11:35:57 +0100

    modified:   Database/database.sql 	modified:   websockets/libs/db/db.go i need to create a global array to put the menu in also need to fina way to use another custom go package

--- websockets/libs/db/db.go
+++ websockets/libs/db/db.go
@@ -1,14 +1,69 @@
 package main
 
 import (
-
 	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 
 )
 
-func test(){
+type menuItem struct {
+
+	name string
+	cat string
+	disc string
+	price float64
+	amount int
 
 
-	fmt.Println("Test")
+}
+
+var menu []menuItems;// i was here i need to create a gloabl array to put the menu in 
+
+func main(){
+
+	db, err := sql.Open("mysql", "tom:pwd123@tcp(127.0.0.1:3306)/cafe_POS_v3")
+
+	if err != nil {
+
+		panic(err.Error())
+
+	}
+	defer db.Close()
+
+	results , err := db.Query("SELECT * FROM `ITEMS` ")
+
+	if err != nil {
+
+		panic(err.Error())
+
+	}
+
+
+
+	for results.Next(){
+
+		var uid string
+		var name string
+		var disc string
+		var price float64
+		var amount int
+
+		err = results.Scan(&uid, &name, &disc, &price, &amount)
+
+		if err != nil{
+
+			panic(err.Error())
+
+		}
+
+		fmt.Println(uid)
+
+	}
+}
+
+func getAllItems(length int){
+
+
 
 }
